5fbbd599af0784f8b216b32f7a622086fbb590c2
jk <jens.kohl@gmail.com>
2014-02-18 14:19:27 +0100

    Add Runscope.com support

--- web/index.php
+++ web/index.php
@@ -60,4 +60,75 @@ $app->post('/slack-proxy/gitlab-push', function(Request $request) use ($app) {
     return $response;
 });
 
+/* Route for runscope */
+$app->post('/slack-proxy/runscope', function(Request $request) use ($app) {
+    $config     = $app['config']['runscope'];
+    $slackUrl   = sprintf('https://%s.slack.com', $app['config']['slack']['team']);
+    $content    = json_decode($request->getContent());
+    $fields     = array();
+
+    if ($config['post_fail_only'] == true && $content->result == 'pass') {
+        return 'Didn\'t do anything. You don\'t want to pass passed tests to slack';
+    }
+
+    $color      = ($content->result == 'pass')
+        ? $config['pass_color']
+        : $config['fail_color'];
+
+    // Config fields
+    $fields[] = [
+        'title' => 'Test name',
+        'value' => $content->test_name,
+        'short' => true
+    ];
+    $fields[] = [
+        'title' => 'Team name',
+        'value' => $content->test_name,
+        'short' => true
+    ];
+    $fields[] = [
+        'title' => 'Bucket name',
+        'value' => $content->bucket_name,
+        'short' => true
+    ];
+    $fields[] = [
+        'title' => 'Overview',
+        'value' => '<'.$content->test_url.'>',
+        'short' => false
+    ];
+    $fields[] = [
+        'title' => 'Details',
+        'value' => '<'.$content->test_run_url.'>',
+        'short' => false
+    ];
+
+    $message_content= ($content->result == 'pass') ? $config['pass_message'] : $config['fail_message'];
+
+    $message = sprintf(
+        $message_content,
+        $content->test_name
+    );
+
+    $params = [
+        'channel'  => '#'.$config['channel'],
+        'username' => $config['username'],
+        'fallback' => $message,
+        'pretext' => $message,
+        'fields'   => $fields,
+        'color'    => $color,
+    ];
+
+    $client  = new Client($slackUrl);
+    $request = $client->post(
+        '/services/hooks/incoming-webhook?token='.$config['token']
+    );
+    $request->setBody(['payload' => json_encode($params)], 'application/x-www-form-urlencoded');
+
+    var_dump(['payload' => json_encode($params)]);
+
+    $response = $request->send();
+
+    return $response;
+});
+
 $app->run();
